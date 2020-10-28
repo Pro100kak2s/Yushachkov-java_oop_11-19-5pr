18937213164459a37b74143d7d8c507388b025e9
Yushachkov Anton <71340057+Pro100kak2s@users.noreply.github.com>
2020-10-28 17:44:11 +0300

    Add files via upload

---
+++ n5.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class n5
+{
+    private static int n=0;
+    public static int rec(int N)
+    {
+        if (N != 0)
+        {
+            N = N/ 10;
+            n=n+1;
+            return rec(N);
+        }
+        if (N==0)
+        {
+            System.out.println(n);
+        }
+        return 0;
+    }
+
+
+    public static void main(String[] args)
+    {
+
+        rec(1738);
+    }
+}
